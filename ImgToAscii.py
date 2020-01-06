60a246a50a47e09f4e97d3f112ad830b73056a8a
vizmiz <47368857+vizmiz@users.noreply.github.com>
2020-01-06 12:22:35 -0700

    Update ImgToAscii.py
    Light and Dark mode files no longer overwrite each other, now have light and dark label.

--- ImgToAscii.py
+++ ImgToAscii.py
@@ -5,16 +5,18 @@ class I2A:
         self.Characters = [' ','.',',','~','+','*','=','%','$','&','#']
         self.Output= ''
         self.FilePath = os.path.dirname(os.path.abspath(__file__))
-
         self.Options()
 
     def Options(self):
         for files in os.listdir('{}/Images'.format(self.FilePath)):
             print(files)
         self.ImageChoice=input("\nWhat is the name of the image you want me to convert?\n Mind you, I'm picky and only like .jpg\n")
-        ModeSelect=input('1 for LightMode(Dark Text on a light background)\n2 for Darkmode(Light text on a dark background)\n')
-        if ModeSelect is '1':
+        self.ModeSelect=input('1 for LightMode(Dark Text on a light background)\n2 for Darkmode(Light text on a dark background)\n')
+        if self.ModeSelect is '1':
             self.Characters.reverse()
+            self.ModeSelect='light'
+        else:
+            self.ModeSelect='dark'
         self.RawImage = Image.open('{0}/Images/{1}.jpg'.format(self.FilePath,self.ImageChoice))
         self.RatioRange()
 
@@ -37,9 +39,10 @@ class I2A:
         self.MRCFile(Ratio)
         self.Output=''
     def MRCFile(self,Ratio):
-        openfile=open("{0}/AsciiArt/{1}:{2}.txt".format(self.FilePath,self.ImageChoice,Ratio),"w") 
+        openfile=open("{0}/AsciiArt/{1}:{2}:{3}.txt".format(self.FilePath,self.ImageChoice,Ratio,self.ModeSelect),"w") 
         print(self.Output,file=openfile)
         openfile.close()
         print('\n\nRatio Level:{}'.format(str(Ratio)))
         print(self.Output)
+        
 I2A()
